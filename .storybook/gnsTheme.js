0317a1eff0fbbaaee5f0fcdaaaa7ee259462e99a
Rafee1100 <rafee.abdullah1@gmail.com>
2023-12-27 14:21:36 +0600

    ims copy

--- .storybook/gnsTheme.js
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming";
-export default create({
-  base: "light",
-  brandTitle: "GNS",
-  brandUrl: "https://www.gnsurveys.co.uk/",
-  brandImage:
-    "https://imssystems.tech/static/assets/images/ims-technologies-official.png",
-  brandTarget: "_self",
-});
